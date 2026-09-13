247643e43a57e41546ce6fee4f662040c84bbae9
Driller34 <dragonhack123@op.pl>
2026-09-13 23:41:51 +0000

    setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="PySnake",
+    version="0.1.0",
+    packages=["src"],
+    install_requires=[
+        "pygame"
+    ],
+    entry_points={
+        "console_scripts": [
+            "pysnake = src.main:main",
+        ],
+    },
+)
