161170f44ac23bd8b0614fc2fc639bc14220b7cc
Driller34 <dragonhack123@op.pl>
2026-09-14 22:27:20 +0000

    feat(layer): add SnakeLayer

--- src/game.py
+++ src/game.py
@@ -1,5 +1,6 @@
 import pygame
 from src.layers.layer_manager import LayerManager
+from src.snake_layer import SnakeLayer
 
 class Game:
     def __init__(self, width : int, height : int, fps : int = 60) -> None:
@@ -8,22 +9,25 @@ class Game:
         self.fps = fps
         self.running = True
         self.layer_manager = LayerManager()
+        self.layer_manager.push_layer(SnakeLayer())
 
     def run(self) -> None:
+        dt = 0
+
         while self.running:
 
             self.eventHandler()
-            self.update()
+            self.update(dt)
             self.render()
 
             pygame.display.flip()
 
-            dt = self.clock.tick(60) / 1000
+            dt = self.clock.tick(self.fps) / 1000
 
         pygame.quit()
 
-    def update(self) -> None:
-        self.layer_manager.update()
+    def update(self, dt) -> None:
+        self.layer_manager.update(dt)
 
     def eventHandler(self) -> None:
         for event in pygame.event.get():
---
+++ src/snake_layer.py
@@ -0,0 +1,14 @@
+from src.layers.layer import Layer
+
+class SnakeLayer(Layer):
+    def __init__(self):
+        pass
+
+    def process_event(self, event : pygame.event.Event):
+        pass
+
+    def update(self, dt):
+        pass
+
+    def render(self):
+        pass
