28f131149293acc03be7631be67ac3e38c8e5488
Driller34 <dragonhack123@op.pl>
2026-09-17 03:22:29 +0000

    fix(game): main loop cear screen after every frame

--- src/game.py
+++ src/game.py
@@ -1,6 +1,6 @@
 import pygame
 from src.layers.layer_manager import LayerManager
-from src.snake.snake_layer import SnakeLayer
+from src.menu.menu_layer import MenuLayer
 
 class Game:
     def __init__(self, config : dict) -> None:
@@ -9,12 +9,13 @@ class Game:
         self.fps = config['window']['fps']
         self.running = True
         self.layer_manager = LayerManager()
-        self.layer_manager.push_layer(SnakeLayer(self.layer_manager, config['game']))
+        self.layer_manager.push_layer(MenuLayer(self.layer_manager, config))
 
     def run(self) -> None:
         dt = 0
 
-        while self.running:
+        while self.running and len(self.layer_manager) > 0:
+            self.screen.fill("black")
 
             self.eventHandler()
             self.update(dt)
@@ -27,9 +28,15 @@ class Game:
         pygame.quit()
 
     def update(self, dt : float) -> None:
+        if len(self.layer_manager) < 1:
+            return
+
         self.layer_manager.update(dt)
 
     def eventHandler(self) -> None:
+        if len(self.layer_manager) < 1:
+            return
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
@@ -37,4 +44,7 @@ class Game:
                 self.layer_manager.process_event(event)
 
     def render(self) -> None:
+        if len(self.layer_manager) < 1:
+            return
+
         self.layer_manager.render(self.screen)
