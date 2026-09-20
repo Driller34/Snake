48ffd8993682add4a86c5ef3cb96752cdd290495
Driller34 <dragonhack123@op.pl>
2026-09-20 02:14:35 +0000

    feat(gui): add Button

---
+++ src/gui/button.py
@@ -0,0 +1,10 @@
+
+class Button:
+    def __init__(self, config : dict, text : str, callback) -> None:
+        pass
+
+    def process_event(self, event : pygame.event.Event) -> None:
+        pass
+
+    def render(self, screen : pygame.Surface) -> None:
+        pass
